7d8a91da0fa0ed9c705db73449831767a201e84a
Petr Kolarik <kolarik.petr@outlook.com>
2023-03-01 16:07:25 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Setup Nextjs Tailwind CSS Styled Components with TypeScript
+
+This is a boilerplate for Nextjs with Tailwind CSS, Styled Components and TypeScript.
+
+## Getting Started
+
+First, clone the repository:
+
+```bash
+git clone https://github.com/MKAbuMattar/nextjs-tailwind-styled-components-typescript.git
+
+cd nextjs-tailwind-styled-components-typescript
+```
+
+Then, install the dependencies:
+
+```bash
+npm yarn
+```
+
+Finally, run the development server:
+
+```bash
+yarn dev
+```
---
+++ src/pages/_app.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+import 'tailwindcss/tailwind.css';
+
+const _App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Nextjs Tailwind CSS Styled Components with TypeScript</title>
+        <meta
+          name="description"
+          content="Nextjs Tailwind CSS Styled Components with TypeScript"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default _App;
