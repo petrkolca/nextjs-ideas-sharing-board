e26c24df7613b3aa0f794efe08e961c64c40bd6a
Petr Kolarik <kolarik.petr@outlook.com>
2023-03-04 18:43:29 +1100

    Getting all posts from database, orderedBy date

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,37 @@
 import Head from "next/head";
 import Message from "../components/Message";
 import { Fragment, useState, useEffect } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../utils/firebase";
 
 //styles
 import { Section, Title } from "../styles/Home.styled";
-import { db } from "../utils/firebase";
 
 const HomePage = () => {
+  // Create a state with all the POSTS
+  const [allPosts, setAllPosts] = useState([]);
+
+  // retrieving all posts from the database
+  const getPosts = async () => {
+    const collectionRef = collection(db, "posts");
+    const q = query(collectionRef, orderBy("timestamp", "desc"));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setAllPosts(
+        snapshot.docs.map((doc) => {
+          // return object of all posts
+          return { ...doc.data() };
+        })
+      );
+    });
+
+    return unsubscribe;
+  };
+
+  useEffect(() => {
+    console.log("all posts are listed: ", allPosts);
+  }, [allPosts]);
+
   return (
     <Fragment>
       <Head>
