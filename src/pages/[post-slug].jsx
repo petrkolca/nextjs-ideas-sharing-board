8ff6a2706325c7b9538e98ea43e5b351d9d0778a
Petr Kolarik <kolarik.petr@outlook.com>
2023-03-07 21:51:28 +1100

    Listing Comments retrieved from the database

--- src/pages/[post-slug].jsx
+++ src/pages/[post-slug].jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useEffect, useState, useCallback, useMemo } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { auth, db } from "../utils/firebase";
@@ -14,7 +15,11 @@ import { toast } from "react-toastify";
 import Message from "../components/Message";
 
 import { Button } from "../styles/Home.styled";
-import { CommentCtn, CommentsListing } from "../styles/Message.styled";
+import {
+  CommentCtn,
+  CommentsListing,
+  UserProfileImg,
+} from "../styles/Message.styled";
 
 const PostDetails = () => {
   // const [user, loading] = useAuthState(auth);
@@ -28,49 +33,88 @@ const PostDetails = () => {
   };
 
   // Submit comment
-  const sendCommentHandler = async (e) => {
+  const sendCommentHandler = async () => {
     console.log("comment submitted");
-    // check if user is NOT logged-in => redirect
-    if (!auth.currentUser) return route.push("/auth/login");
 
-    // notify user if comment field is empty
-    if (!comment) {
-      toast.error("Don't leave empty comment textarea!", {
+    try {
+      // check if user is NOT logged-in => redirect
+      if (!auth.currentUser) {
+        await route.push("/auth/login");
+        throw new Error("User is not logged in.");
+      }
+
+      // notify user if comment field is empty
+      if (!comment) {
+        throw new Error("Comment field is empty.");
+      }
+
+      const docRef = doc(db, "posts", routeData.id);
+      // updasting current post data
+      // by submitting into Post database and creating new ARRAY
+      // with arrayUnion function
+      await updateDoc(docRef, {
+        comments: arrayUnion({
+          message: comment,
+          avatar: auth.currentUser.photoURL,
+          username: auth.currentUser.displayName,
+          time: Timestamp.now(),
+        }),
+      });
+
+      setComment("");
+    } catch (error) {
+      toast.error(error.message, {
         position: toast.POSITION.TOP_CENTER,
         autoClose: 1000,
       });
-
-      return;
     }
-
-    const docRef = doc(db, "posts", routeData.id);
-    // updasting current post data
-    // by submitting into Post database and creating new ARRAY
-    // with arrayUnion function
-    await updateDoc(docRef, {
-      comments: arrayUnion({
-        message: comment,
-        avatar: auth.currentUser.photoURL,
-        username: auth.currentUser.displayName,
-        time: Timestamp.now(),
-      }),
-    });
-
-    setComment("");
   };
 
-  const getComments = async () => {
+  // useCallback function as it is dependancy for useEffect
+  // and called only on useEffect request
+  const getComments = useCallback(async () => {
     const docRef = doc(db, "posts", routeData.id);
     // retrieving data from current post.id
     const docSnapshot = await getDoc(docRef);
-    // setting all comments retreived from DATA of the current post.id
-    setAllComments(docSnapshot.data().comments);
-  };
+    // check if the snapshot exists
+    if (docSnapshot.exists()) {
+      // setting all comments retreived from DATA of the current post.id
+      setAllComments(docSnapshot.data().comments);
+    }
+  }, [routeData.id]);
 
   useEffect(() => {
     if (!route.isReady) return;
     getComments();
-  }, []);
+  }, [route.isReady, getComments]);
+
+  // useMemo function memorise the computed value of the Comments Listing
+  const commentsListing = useMemo(() => {
+    return allComments.map((comment, index) => {
+      return (
+        <div key={index}>
+          <UserProfileImg>
+            {comment.avatar.length > 0 && (
+              <Image
+                src={comment.avatar}
+                alt={comment.username}
+                layout="fill" // required
+                objectFit="cover" // change to suit your needs
+                className=""
+              />
+            )}
+          </UserProfileImg>
+          <h2>{comment.username}</h2>
+          <div>
+            <p>{comment.message}</p>
+          </div>
+        </div>
+      );
+    });
+  }, [allComments]);
+
+  // console.log("routeData: ", route.isReady);
+  if (!route.isReady) return;
 
   return (
     <div>
@@ -90,15 +134,7 @@ const PostDetails = () => {
         </div>
         <CommentsListing>
           <h2>Comments</h2>
-          {allComments.map((comment) => {
-            return (
-              <div>
-                <div>
-                  
-                </div>
-              </div>
-            );
-          })}
+          {commentsListing}
         </CommentsListing>
       </CommentCtn>
     </div>
